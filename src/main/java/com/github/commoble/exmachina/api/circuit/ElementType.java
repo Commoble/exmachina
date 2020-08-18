2b7a0428e6462a787b012df1be9bb959d6771f27
Commoble <josephbettendorff@yahoo.com>
2020-08-18 15:26:58 -0500

    Yeet everything in preparation for new circuit framework

--- src/main/java/com/github/commoble/exmachina/api/circuit/ElementType.java
+++
@@ -1,8 +0,0 @@
-package com.github.commoble.exmachina.api.circuit;
-
-public enum ElementType
-{
-	RESISTOR,
-	VOLTAGE_SOURCE,
-	CURRENT_SOURCE	// not implemented yet
-}
--- src/main/java/com/github/commoble/exmachina/content/util/MathBuddy.java
+++
@@ -1,6 +0,0 @@
-package com.github.commoble.exmachina.content.util;
-
-public class MathBuddy
-{
-	public static final float DEG2RAD = (float) (Math.PI / 180D);
-}
